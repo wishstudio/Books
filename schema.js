0b5db9352d54fd919b0dae8c08bf8df0bae748a6
Xiangyan Sun <wishstudio@gmail.com>
2014-05-12 09:11:16 +0800

    Some unsigned constraints.

--- schema.js
+++ schema.js
@@ -27,7 +27,7 @@ exports.initialize = function() {
         table.string('realname', 30);
         table.string('uid', 50);
         table.boolean('gender');
-        table.integer('age');
+        table.integer('age').unsigned();
         table.dateTime('registrationTime').notNullable();
         table.integer('power').notNullable();
     }, User, {
@@ -48,14 +48,14 @@ exports.initialize = function() {
             table.string('publisher', 50);
             table.string('author', 30);
             table.decimal('price').notNullable();
-            table.integer('stock').defaultTo(0).notNullable();
+            table.integer('stock').defaultTo(0).unsigned().notNullable();
         });
     }).then(function() {
         return createTable('orders', function(table) {
             table.increments('id').primary();
             table.integer('bookId').notNullable();
             table.string('upstream').notNullable();
-            table.integer('count').notNullable();
+            table.integer('count').unsigned().notNullable();
             table.decimal('uprice').notNullable();
             table.integer('status').notNullable();
         });
